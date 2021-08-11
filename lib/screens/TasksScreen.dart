a8ee3407a9220b29ec74453253d84305e05d97e4
HappySinha <53011602+HappySinha@users.noreply.github.com>
2021-08-11 21:02:19 +0530

    First Commit

---
+++ lib/screens/TasksScreen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      size: 32,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    backgroundColor: Colors.white,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Todoey',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text('12 Tasks', style: TextStyle(color: Colors.white)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.horizontal(
+                    left: Radius.circular(20),
+                    right: Radius.circular(20),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
